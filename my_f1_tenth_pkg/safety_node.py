c4145257391936499aa303d387a9c54ad92a7654
AlexanderFurman <alexfurman16@.com>
2022-11-10 19:23:10 +0200

    added if

--- my_f1_tenth_pkg/safety_node.py
+++ my_f1_tenth_pkg/safety_node.py
@@ -32,6 +32,7 @@ class Safety(Node):
         self.odom_sub_ = self.create_subscription(Odometry, '/ego_racecar/odom', self.odom_callback, 10)
         self.scan_sub_ = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
         self.drive_pub_ = self.create_publisher(AckermannDriveStamped, '/cmd_vel', 10)
+        self.epsilon = 0.01
 
     def odom_callback(self, odom_msg):
         self.speed = odom_msg.twist.twist.linear.x
@@ -42,18 +43,13 @@ class Safety(Node):
         angle_max = scan_msg.angle_max
         ranges = np.array(scan_msg.ranges)
         cos_theta = np.cos(np.linspace(angle_min, angle_max, num=len(ranges)))
-        # print(angle_min,angle_max,ranges,cos_theta)
-        for item in cos_theta:
-            print(item)
-        time_to_collision = np.argmin(np.abs(ranges/(self.speed*cos_theta)))
+        if abs(self.speed) > self.epsilon:
+            time_to_collision = np.argmin(np.abs(ranges/(self.speed*cos_theta)))
 
-        if time_to_collision <= 1:
-            vel = AckermannDriveStamped()
-            vel.drive.speed = 0
-            self.drive_pub_.publish(vel)
-
-        # TODO: publish brake message and publish controller bool
-
+            if time_to_collision <= 1:
+                vel = AckermannDriveStamped()
+                vel.drive.speed = 0
+                self.drive_pub_.publish(vel)
 
 def main(args=None):
     rclpy.init(args=args)
