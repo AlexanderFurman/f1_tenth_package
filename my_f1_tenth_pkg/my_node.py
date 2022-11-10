927c1c42ea1a71fd9c136d54387bb091d194df9e
AlexanderFurman <alexfurman16@.com>
2022-11-10 13:08:37 +0200

    initial commit

---
+++ my_f1_tenth_pkg/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from my_f1_tenth_pkg.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'my_f1_tenth_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alex',
+    maintainer_email='alexfurman16@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = my_f1_tenth_pkg.my_node:main'
+        ],
+    },
+)
